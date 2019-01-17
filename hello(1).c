c6b646533a1d375bbe15117cc0ec994fc7ef70e0
kamare2 <abdklt6@gmail.com>
2019-01-17 15:19:24 -0600

    Update hello(1).c

--- hello(1).c
+++ hello(1).c
@@ -1,9 +1,9 @@
 /**
- * Author: Han Ang
+ * Author: Han Ang was the author
  * Date: 2019/01/15
  *
  * A simple hello world program in C
- *
+ * Steals his roommate's shit litteraly 
  */
 #include<stdlib.h>
 #include<stdio.h>
