669804df0c533893cf32de28a214289e2fd01316
KelvinAng1999 <hanweiang88@gmail.com>
2019-01-17 14:55:12 -0600

    Initial commit

---
+++ hello(1).c
@@ -0,0 +1,16 @@
+/**
+ * Author: Han Ang & Kaleab Amare Mengistu
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Han Ang & Kaleab Amare Mengistu\n");
+
+  return 0;
+}
